ad79b17ed7a1fe0d1dc5da7c5351fe1f7c33f394
Danilov Nikita <danilovn228@gmail.com>
2026-02-01 00:37:42 +0500

    Add files via upload

---
+++ lists/list2.py
@@ -0,0 +1,17 @@
+numbers = [3, 4, 5]
+# numbers[3] = 100
+numbers.append(100)
+numbers.insert(1, True)
+
+b = [6, 7, 8]
+numbers.extend(b)
+# numbers.reverse()
+numbers.sort()
+
+numbers.pop(-2)
+numbers.remove(6)
+
+# numbers.clear()
+
+# print(numbers.count(True))
+# print(len(numbers))
---
+++ lists/list3.py
@@ -0,0 +1,5 @@
+nums = [5, 2, 7, "50", False]
+
+for el in nums:
+    el *= 2
+    print(el)
